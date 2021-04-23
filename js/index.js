0711ab644d8128a77f669ac6b4a348d032a8c2a1
LukeDylanLeo <orane.bouy.intartaglia@wanadoo.fr>
2021-04-23 14:12:03 +0200

    finitions en js + menu et infos dans page accuei lycée

--- js/index.js
+++ js/index.js
@@ -1,3 +1,14 @@
-$("#button").on("click", function() {
-  $(".fin").fadeIn();
+$(function() {
+  $("#buttonup").hide();
+  $("#button").on("click", function() {
+    $(".fin").fadeIn(1000);
+    $("#buttonup").show();
+    $("#button").hide();
+  })
+
+  $("#buttonup").on("click", function(){
+    $(".fin").fadeOut(700);
+    $("#buttonup").hide();
+    $("#button").show();
+  })
 })
