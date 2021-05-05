8caab554e0d9762e796c17cd0a41b9aad602f58b
LukeDylanLeo <orane.bouy.intartaglia@wanadoo.fr>
2021-05-05 16:49:08 +0200

    j'ai oublié ce que j'avais fait

--- lycee/accueil_organisation.php
+++ lycee/accueil_organisation.php
@@ -1,87 +1,83 @@
 <?php
 $datas = [
-  "sciences" => [
-    "titre" => "Sciences",
-    "icone" => "science.svg",
+  "article1" => [
+    "titre" => "Bienvenue sur le site de la cité scolaire Triboulet",
     "text" => "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
     "link" => "#",
   ],
-  "social" => [
-    "titre" => "Sciences humaines",
-    "icone" => "social.svg",
+  "article2" => [
+    "titre" => "Qui est Albert Triboulet?",
     "text" => "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
     "link" => "#",
   ],
-  "litterature" => [
-    "titre" => "Littérature",
-    "icone" => "literature.svg",
+  "article3" => [
+    "titre" => "Origines de l'établissement",
     "text" => "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
     "link" => "#",
   ],
-  "langues" => [
-    "titre" => "Langues",
-    "icone" => "langues.svg",
+  "article4" => [
+    "titre" => "Pour vous rendre au lycée Albert Triboulet",
     "text" => "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
     "link" => "#",
   ],
-  "eps" => [
-    "titre" => "Education physique & sportive",
-    "icone" => "sport.svg",
+  "article5" => [
+    "titre" => "Quelques perspectives du lycée depuis sa rénovation",
     "text" => "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
     "link" => "#",
   ],
-  "international" => [
-    "titre" => "Europe & International",
-    "icone" => "international.svg",
+  "article6" => [
+    "titre" => "Quelques photos de 2010-2011",
     "text" => "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
     "link" => "#",
-  ],
-  "interdisciplinaire" => [
-    "titre" => "Activités interdisciplinaires",
-    "icone" => "other.svg",
-    "text" => "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-    "link" => "#",
-  ],
+   ],
 ];
-?>
+ ?>
 
 <!DOCTYPE html>
 <html lang="fr" dir="ltr">
 <head>
   <meta charset="utf-8">
-  <link rel="stylesheet" href="../css/all.min.css">
   <link rel="stylesheet" href="../css/accueil.css">
+  <link rel="stylesheet" href="../css/all.min.css">
+  <link rel="stylesheet" href="../css/organisation.css">
   <link rel="stylesheet" href="../css/menu_item.css">
   <title>Cité scolaire - Lycée Triboulet</title>
 </head>
 <body>
   <div class="all">
-    <?php include 'head.html'; ?>
+<?php include 'head.html'; ?>
     <div class="content">
       <div class="left">
+
         <?php include 'menu.html'; ?>
       </div>
       <div class="right">
-        <div class="choix">
+<div class="infos">
+  <h1 class="info generale">Infos générales: le lycée en bref</h1>
+</div>
 
-          <?php
-          foreach ($datas as $data) {?>
-            <div class="cartes matieres">
-              <h3>
-                <?php echo $data["titre"]  ?>
-              </h3>
-              <img src="../img/<?php echo $data['icone'] ?>" alt="Image">
-              <p><?php echo $data['text'] ?></p>
-              <a href="<?php echo $data["link"] ?>">
-                <button type="button" name="button"><i class="fas fa-eye"></i> Voir plus</button>
-              </a>
-            </div>
-          <?php }  ?>
+<div class="choix">
 
-        </div>
+<?php
+foreach ($datas as $data) {?>
+  <div class="cartes articles">
+  <div class="contenu">
+    <h3>
+      <?php echo $data["titre"]  ?>
+    </h3>
+    <p><?php echo $data['text'] ?></p>
+  </div>
+    <a href="<?php echo $data["link"] ?>">
+      <button type="button" name="button"><i class="fas fa-eye"></i> Voir plus</button>
+    </a>
+  </div>
+<?php }  ?>
+
+</div>
       </div>
     </div>
     <footer>
+      <p></p>
     </footer>
   </div>
 </body>
